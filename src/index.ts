c6bc20876ed6c9d99eab1f594978c2bf4da7708c
mmert9008 <michael.mertens.jr@gmail.com>
2026-09-01 09:16:43 -0500

    ready to submit CH2 L1

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello, World!");
+function main() {
+  const args = process.argv.slice(2);
+
+  if (args.length < 1) {
+    console.error("error: no website provided");
+    process.exit(1);
+  }
+
+  if (args.length > 1) {
+    console.error("error: too many arguments provided");
+    process.exit(1);
+  }
+
+  const baseURL = args[0];
+  console.log(`Starting crawl of ${baseURL}`);
+  process.exit(0);
+}
+
+main();
